c873d401c8e32a5f9aaaa073f913b9ffda30db88
kelvins insua <kelvinsinsua@gmail.com>
2020-10-11 19:07:04 -0400

    first commit

--- src/main/java/com/interfell/demo/DemoController.java
+++ src/main/java/com/interfell/demo/DemoController.java
@@ -7,6 +7,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
